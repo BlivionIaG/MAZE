3e27a5f6f174ce359243f80bf0e9dd7511fa34d9
BlivionIaG <kev29lt@gmail.com>
2017-04-17 14:22:42 +0200

    modifié :         src/maze_generator.c 	modifié :         src/maze_solver.c

--- src/maze_generator.c
+++ src/maze_generator.c
@@ -1,83 +1,83 @@
 #include "mazemaker.h"
 
-int main(int argc, char **argv){
-	if(argc != 5){
-		printf("USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n");
-		exit(-1);
+int main( int argc, char **argv ){
+	if( argc != 5 ){
+		printf( "USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n" );
+		exit( -1 );
 	}else{
-		if(!atoi(argv[2])){
-		printf("USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n");
-			exit(-2);
-		}else if(!atoi(argv[3])){
-		printf("USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n");
-			exit(-2);
+		if( !atoi( argv[2] ) ){
+		printf( "USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n" );
+			exit( -2 );
+		}else if( !atoi( argv[3] ) ){
+		printf( "USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n" );
+			exit( -2 );
 		}
 	}
-	if((int)atoi(argv[2])/3 != (double)atoi(argv[2])/3){
-			printf("USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n");
-			exit(-9);
+	if( (int) atoi( argv[2] )/3 != (double) atoi( argv[2] )/3 ){
+			printf( "USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n" );
+			exit( -9 );
 	}
-	if((int)atoi(argv[3])/3 != (double)atoi(argv[3])/3){
-			printf("USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n");
-			exit(-9);
+	if( (int) atoi( argv[3] )/3 != (double) atoi( argv[3])/3 ){
+			printf( "USAGE : ./maze_generator filename width height genAlgorithm ( width and height are multiples of 3 )\n" );
+			exit( -9 );
 	}
 
 
-	FILE *file = fopen(argv[1], "w");
-	if(file == NULL){
-		printf("ERROR: cannot create %s\n", argv[1] );
-		exit(-3);
+	FILE *file = fopen( argv[1], "w" );
+	if( file == NULL ){
+		printf( "ERROR: cannot create %s\n", argv[1] );
+		exit( -3 );
 	}
-	srand(time(NULL));
+	srand( time( NULL ) );
 
-	fprintf(file,"%s\n", argv[2]);
-	int width = atoi(argv[2]);
-	int height = atoi(argv[3]);
+	fprintf( file, "%s\n", argv[2] );
+	int width = atoi( argv[2] );
+	int height = atoi( argv[3] );
 	int i;
 
-	char **map = malloc(height*sizeof(char *));
-	for(i=0; i<height; i++){
-		map[i] = malloc((width+1)*sizeof(char));
+	char **map = malloc( height*sizeof(char *) );
+	for( i = 0; i < height; i++ ){
+		map[i] = malloc( (width+1)*sizeof(char) );
 	}
 
-	for(int j=0; j<height; j++){
-		for(int k=0; k<width; k++){
+	for( int j = 0; j < height; j++ ){
+		for( int k = 0; k < width; k++ ){
 			map[j][k] = '1';
 		}
 		map[j][width] = 0;
 	}
 
-	switch(atoi(argv[4])){
+	switch( atoi( argv[4] ) ){
 		case 1:
-			printf("StupidGen method selected\n");
-			stupidGen(map, width, height);
+			printf( "StupidGen method selected\n" );
+			stupidGen( map, width, height );
 		break;
 		default:
-			printf("PerfectGen method selected\n");
-			perfectGen(map, width, height);
+			printf( "PerfectGen method selected\n" );
+			perfectGen( map, width, height );
 		break;
 	}
 
 	/* WRITE */
-	for(i=0; i<height; i++){
-		fprintf(file, "%s\n", map[i]);
+	for( i = 0; i < height; i++ ){
+		fprintf( file, "%s\n", map[i] );
 	}
 
 	/* FREE and CLOSE */
-	for(i=0; i<height; i++){
-		free(map[i]);
+	for( i = 0; i < height; i++ ){
+		free( map[i] );
 	}
-	free(map);
-	fclose(file);
+	free( map );
+	fclose( file );
 
 	return 0;
 }
 
-void *malloc_safe(size_t size)
-{
-    void* ptr = malloc(size);
-    if(ptr == NULL)
-        return malloc_safe(size); 
-    else
-        return ptr;
+void *malloc_safe( size_t size ){
+	void* ptr = malloc( size );
+	if( ptr == NULL ){
+		return malloc_safe(size);
+	}else{
+        	return ptr;
+	}
 }
--- src/maze_solver.c
+++ src/maze_solver.c
@@ -1,108 +1,108 @@
 #include "lymbe.h"
 
-int main(int argc, char **argv){
-	if(argc!=2) return -1; //If no file specified
+int main( int argc, char **argv ){
+	if( argc != 2 ) return -1; //If no file specified
 
-	FILE *labyrinthe = fopen(argv[1], "r"); //Open maze file
-	if(labyrinthe == NULL) { //If failed, exit
-		printf("ERREUR donne un fichier qui existe !!!\n");
+	FILE *labyrinthe = fopen( argv[1], "r" ); //Open maze file
+	if( labyrinthe == NULL ) { //If failed, exit
+		printf( "ERREUR donne un fichier qui existe !!!\n" );
 		return -2;
 	}
 
-	char *taille = malloc( (MAX_VAL+1)*sizeof(char) ); 
+	char *taille = malloc( ( MAX_VAL + 1 )*sizeof(char) ); 
 	int largeur = 0;
 
-	if(!fgets(taille, MAX_VAL, labyrinthe)){
-		printf("ERREUR le fichier est invalide !\n");
+	if( !fgets( taille, MAX_VAL, labyrinthe ) ){
+		printf( "ERREUR le fichier est invalide !\n" );
 		return -3;
 	}else{
-		largeur = atoi(taille);
-		printf("largeur:%d\n", largeur);
-		free(taille);
-		taille = malloc((largeur+1)*sizeof(char));
+		largeur = atoi( taille );
+		printf( "largeur:%d\n", largeur );
+		free( taille );
+		taille = malloc( ( largeur + 1 )*sizeof(char) );
 	}
 
 	char **terrain = NULL;
 	int terrainLength = 0;
 
-	while(fgets(taille, largeur+2, labyrinthe) ){
-		taille[strlen(taille)] = 0;
+	while( fgets( taille, largeur + 2, labyrinthe ) ){
+		taille[strlen( taille )] = 0;
 
 		terrainLength++;
-		terrain = realloc( terrain, terrainLength*sizeof(char *));
-		terrain[terrainLength-1] = malloc((largeur+1)*sizeof(char));
-		strcpy(terrain[terrainLength-1], taille);
+		terrain = realloc( terrain, terrainLength*sizeof(char *) );
+		terrain[terrainLength-1] = malloc( ( largeur + 1 )*sizeof(char) );
+		strcpy( terrain[terrainLength-1], taille );
 	}
-	printf("terrainLength:%d\n", terrainLength);
+	printf( "terrainLength:%d\n", terrainLength );
 
 	int lw = BLOCK_SIZE, lh = BLOCK_SIZE;
 	int tw = largeur*BLOCK_SIZE, th = terrainLength*BLOCK_SIZE;
 
-	SSDL *window = SSDL_Init( tw, th, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
-	SSDL_Background( window, RGB( window->ecran,192, 192, 192 ) );
-	set_Title("Labyrinthe");
+	SSDL *window = SSDL_Init( tw, th, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE );
+	SSDL_Background( window, RGB( window->ecran, 192, 192, 192 ) );
+	set_Title( "Labyrinthe" );
 
 	bot *boy = init_bot(
-		findFirstCharX(terrain, terrainLength, '2'),
-		findFirstCharY(terrain, terrainLength, '2'),
+		findFirstCharX( terrain, terrainLength, '2' ),
+		findFirstCharY( terrain, terrainLength, '2' ),
 		lw,
 		lh,
-		RGB(window->ecran, 255,255,0),
-		RGB(window->ecran, 0,255,255),
-		RGB(window->ecran, 255,0,255)
+		RGB( window->ecran, 255, 255, 0 ),
+		RGB( window->ecran, 0, 255, 255 ),
+		RGB( window->ecran, 255, 0, 255 )
 	);
 
-	Uint32 last_time = SDL_GetTicks();
-    Uint32 current_time,ellapsed_time;
-    Uint32 start_time;
-    SDL_Event event;
+	Uint32 last_time = SDL_GetTicks( );
+	Uint32 current_time,ellapsed_time;
+	Uint32 start_time;
+	SDL_Event event;
 
-    draw_map(window, terrain, largeur, terrainLength, lw, lh );
-	draw_bot(window, boy);
-	SSDL_Print(window);
+	draw_map( window, terrain, largeur, terrainLength, lw, lh );
+	draw_bot( window, boy );
+	SSDL_Print( window );
 
-	while('s' != SSDL_Wait4KeyboardPress() );
+	while( 's' != SSDL_Wait4KeyboardPress( ) );
 
-	while(!boy->finished){
-		start_time = SDL_GetTicks();
+	while( !boy->finished ){
+		start_time = SDL_GetTicks( );
 		/* I/O */
-		while (SDL_PollEvent(&event)){
-			switch(event.type){
-            	case SDL_QUIT:
-					SSDL_Free(window);
-					quit();
-					exit(0);
-            	break;
-            	case SDL_KEYDOWN:
-                	switch(event.key.keysym.sym){
-	                    case 'q':
-							SSDL_Free(window);
-							quit();
-							exit(0);
-	                    break;
-	                    case 'p':
-	                    	boy->wSize++;
-	                    	boy->hSize++;
-	                    	lw++;
-	                    	lh++;
-	                    break;
-	                    case 'm':
-	                    	if(boy->wSize > 0) boy->wSize--;
-	                    	if(boy->hSize > 0) boy->hSize--;
-	                    	if(lw > 0) lw--;
-	                    	if(lh > 0) lh--;
-	                    break;
-	                    default:
-	                    break;
-                	}
-            	break;
-        	}
+		while( SDL_PollEvent( &event ) ){
+			switch( event.type ){
+				case SDL_QUIT:
+					SSDL_Free( window );
+					quit( );
+					exit( 0 );
+				break;
+				case SDL_KEYDOWN:
+					switch( event.key.keysym.sym ){
+						case 'q':
+							SSDL_Free( window );
+							quit( );
+							exit( 0 );
+						break;
+						case 'p':
+							boy->wSize++;
+							boy->hSize++;
+							lw++;
+							lh++;
+						break;
+						case 'm':
+							if( boy->wSize > 0 ) boy->wSize--;
+							if( boy->hSize > 0 ) boy->hSize--;
+							if( lw > 0 ) lw--;
+							if( lh > 0 ) lh--;
+						break;
+						default:
+						break;
+					}
+				break;
+			}
 		}
-		current_time = SDL_GetTicks();
-        ellapsed_time = current_time - last_time;
-        last_time = current_time;
+		current_time = SDL_GetTicks( );
+		ellapsed_time = current_time - last_time;
+		last_time = current_time;
 
-        /* DRAW */
+		/* DRAW */
 		SSDL_Clear(window);
 		draw_map(window, terrain, largeur, terrainLength, lw, lh );
 		draw_bot(window, boy);
@@ -111,9 +111,9 @@ int main(int argc, char **argv){
 
 		/* END of Loop */
 		ellapsed_time = SDL_GetTicks() - start_time;
-        if (ellapsed_time < PRGM_DELAY){
-            SDL_Delay(PRGM_DELAY - ellapsed_time);
-        }
+		if (ellapsed_time < PRGM_DELAY){
+			SDL_Delay(PRGM_DELAY - ellapsed_time);
+		}
 	}
 
 	printf("PATH FOUNDED !!!\n");
