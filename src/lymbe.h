ea6c67a58a2eb5ade60c03bf03fc28db62379f5b
BlivionIaG <kev29lt@gmail.com>
2017-04-17 15:16:07 +0200

    modifié :         src/lymbe.h

--- src/lymbe.h
+++ src/lymbe.h
@@ -9,31 +9,40 @@
 #define FALSE 0
 #define TRUE 1
 
+/* Struct for coordinates */
 typedef struct bot_coords {
 	int x, y;
 } bot_coords;
 
+/* Struct for bot memory */
 typedef struct bot_memory {
 	int *x, *y;
 	int *left, *right, *up, *down;
 	int length;
 } bot_memory;
 
+/* Struct for a bot */
 typedef struct bot {
 	int x, y, wSize, hSize;
 	int left, right, up, down;
 	bot_memory *nodes;
-	bot_coords *memory;
-	int length;
+	bot_coords *memory;  // memory stack
+	int length; // memory stack length
 	Uint32 color, oldColor, nodeColor;
 
-	int finished;
+	int finished; //If bot is at the end of the maze
 } bot;
 
+/* Initialize a bot */ 
 bot *init_bot( int x, int y, int wSize, int hSize, Uint32 color, Uint32 nc, Uint32 oc );
+/* Initialize a bot memory */
 bot_memory *init_bot_memory( );
+
+/* Push current coordinates to memory stack */
 void bot_memory_push( bot *in );
+/* Pop last coordinates from memory stack to current coordinates */
 void bot_memory_pop( bot *in );
+
 void bot_nodes_push( bot *in );
 int already_been_there( bot *in, int x, int y );
 int position_in_bot_nodes( bot_memory *nodes, int x, int y );
